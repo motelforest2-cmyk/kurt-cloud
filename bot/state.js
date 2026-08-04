f66389748ae65ce5b06d3298688d00495fe93abc
motelforest2-cmyk <motelforest2@gmail.com>
2026-08-04 14:05:39 +0200

    Create state.js

---
+++ bot/state.js
@@ -0,0 +1,8 @@
+export const state = {
+  qr: null,
+  pairingCode: null,
+  paired: false,
+  status: "waiting", // waiting | paired | install_auto | install_manual | ready
+  serverId: null,
+  botOnline: false
+};
