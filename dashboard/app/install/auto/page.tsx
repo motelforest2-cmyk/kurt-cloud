3ccf7642ebae5563a00e7ac6bba50b96f81ac414
motelforest2-cmyk <motelforest2@gmail.com>
2026-08-05 18:54:06 +0200

    Update page.tsx

--- dashboard/app/install/auto/page.tsx
+++ dashboard/app/install/auto/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Navbar from "../../../components/Navbar";
-import InstallAutoCard from "../../../components/InstallAutoCard";
-import { installAuto } from "../../../lib/api";
+import Navbar from "../../../../components/Navbar";
+import InstallAutoCard from "../../../../components/InstallAutoCard";
+import { installAuto } from "../../../../lib/api";
 
 export default function Page() {
   return (
