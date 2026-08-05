c2162d2517c2b07f1f2b131355060dc5bd182c6f
motelforest2-cmyk <motelforest2@gmail.com>
2026-08-05 18:36:53 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 export default function Navbar() {
   return (
-    <header style={{ marginBottom: 20, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-      <h1 className="title">Kurt Cloud Dashboard</h1>
-      <span className="badge">WispByte Cloud</span>
+    <header style={{ marginBottom: 20 }}>
+      <h1 className="title">KurtCloud Dashboard</h1>
     </header>
   );
 }
